091913bc32aecf02dd1faab5ddd487c2e537d571
adam <adamlin120@gmail.com>
2020-09-29 03:40:24 +0800

    prepare for submission

--- gen.sh
+++ gen.sh
@@ -1,3 +1,11 @@
-python make_submission.py ytlin/1cejkgok_1 1cejkgok_1
-python make_submission.py ytlin/1cejkgok_6 1cejkgok_6
-python make_submission.py ytlin/1cejkgok_9 1cejkgok_9
+RUNID="1cejkgok"
+for EPOCH in 1 6 9
+do
+  for SPLIT in test val human_val test data
+  do
+    echo  "ytlin/${RUNID}_${EPOCH}"
+    echo  "${RUNID}_${EPOCH}"
+    echo  "./data/multiwoz/processed/zh/${SPLIT}.json"
+    python make_submission.py "ytlin/${RUNID}_${EPOCH}" "${RUNID}_${EPOCH}" --test_set "./data/multiwoz/processed/zh/${SPLIT}.json"
+  done
+done
