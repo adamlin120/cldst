33e8e1dce4a646467892c0af416596cff5138488
adam <adamlin120@gmail.com>
2020-10-03 02:16:49 +0800

    add arg

--- gen.sh
+++ gen.sh
@@ -1,10 +1,13 @@
-RUNID="1cejkgok"
+RUNID=${1}
+DATASET=${2:-"crosswoz"}
+LANG=${3:-"en"}
+
 for EPOCH in 1 6 9
 do
-  for SPLIT in test val human_val test data
+  for SPLIT in val human_val test data
   do
     CKPT="${RUNID}_${EPOCH}"
-    DATA="./data/multiwoz/processed/zh/${SPLIT}.json"
+    DATA="./data/${DATASET}/processed/${LANG}/${SPLIT}.json"
     echo "${CKPT}"
     echo "${DATA}"
     python make_submission.py "ytlin/${CKPT}" "${CKPT}" --test_set "${DATA}"
