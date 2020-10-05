c30006a2ea83627851d002acbf489de99940f351
adam <adamlin120@gmail.com>
2020-10-06 04:20:24 +0800

    no hparam

--- save_transformer_model_tokenizer.py
+++ save_transformer_model_tokenizer.py
@@ -11,20 +11,16 @@ logging.basicConfig(level=logging.INFO)
 def main():
     args = parse_args()
     module_class = MBartDST if args.mbart else ConditionalLM
-    model = module_class.load_from_checkpoint(
-        checkpoint_path=args.checkpoint_path,
-        hparams_file=args.hparams_file,
-    )
-    args.save_dir.mkdir(exist_ok=True, parents=True)
-    model.model.save_pretrained(args.save_dir)
-    model.tokenizer.save_pretrained(args.save_dir)
+    model = module_class.load_from_checkpoint(checkpoint_path=str(args.checkpoint_path))
+    save_dir = args.checkpoint_path / args.checkpoint_path.stem
+    save_dir.mkdir(exist_ok=True, parents=True)
+    model.model.save_pretrained(save_dir)
+    model.tokenizer.save_pretrained(save_dir)
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser(add_help=False)
-    parser.add_argument("checkpoint_path")
-    parser.add_argument("hparams_file")
-    parser.add_argument("save_dir", type=Path)
+    parser.add_argument("checkpoint_path", type=Path)
     parser.add_argument("--mbart", action="store_true")
     return parser.parse_args()
 
